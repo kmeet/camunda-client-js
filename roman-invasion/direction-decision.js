8235a085dcd721f1d4b26ba1c1266b9d93a39c40
Meet Kulkarni <kulkarni.meet@gmail.com>
2020-06-10 21:39:42 -0400

    Initial commit

---
+++ roman-invasion/direction-decision.js
@@ -0,0 +1,16 @@
+const { Client, logger } = require("camunda-external-task-client-js");
+
+// configuration for the Client:
+//  - 'baseUrl': url to the Workflow Engine
+//  - 'logger': utility to automatically log important events
+const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
+
+// create a Client instance with custom configuration
+const client = new Client(config);
+
+// susbscribe to the topic: 'creditScoreChecker'
+client.subscribe("creditScoreChecker", async function({ task, taskService }) {
+  // Put your business logic
+  // complete the task
+  await taskService.complete(task);
+});
