bbe7d105bc874253837fca100b63e16fb55ea3bd
Meet Kulkarni <kulkarni.meet@gmail.com>
2020-06-11 00:35:38 -0400

    Minor logging code

--- roman-invasion/direction-decision.js
+++ roman-invasion/direction-decision.js
@@ -10,10 +10,11 @@ const config = { baseUrl: "http://localhost:8080/engine-rest", use: logger };
 const client = new Client(config);
 const processVariables = new Variables();
 
-// susbscribe to the topic: 'creditScoreChecker'
+// susbscribe to the topic: 'DecideOnRomanExpansion'
 client.subscribe("DecideOnRomanExpansion", async function ({ task, taskService }) {
     // Put your business logic
-    var north = Math.random() > 0.5;
+    var north = Math.random() >= 0.5;
+    console.log(north);
     processVariables.set("north", north);
     // complete the task
     await taskService.complete(task, processVariables);
